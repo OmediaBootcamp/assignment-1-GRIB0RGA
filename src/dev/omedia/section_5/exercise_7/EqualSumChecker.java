29c0216f6a70c186f9bca4c7d175af3497b33c1f
Griborga <giorgi4@discovery.co.za>
2024-09-25 21:23:23 +0400

    [Section - 5][Exercise - 7] - EqualSumChecker

---
+++ src/dev/omedia/section_5/exercise_7/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package dev.omedia.section_5.exercise_7;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int int1, int int2, int sum) {
+        return int1 + int2 == sum;
+    }
+}
